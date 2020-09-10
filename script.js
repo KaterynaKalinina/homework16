944b6adec60ea11c75a4c858079280aee00ac52f
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-09-10 11:45:13 +0300

    homeowrk16

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const aviation = new MilitaryResource('aviation', 100, 130, 0, 1000);
-console.log(aviation);
-aviation.isReadyToMove();
-aviation.isReadyToFight();
-aviation.restore();
+const units = [new Unit('Virus', 200, 500)];
 
-const infantry = new MilitaryResource();
-console.log(infantry);
-//  infantry.clone(aviation);
+console.log(units[0].isReadyToFight());
+console.log(units[0].isReadyToMove());
+units.push(units[0].clone());
+console.log(units);
+
+const squad = new Squad(units);
+console.log(squad);
+console.log(squad.isReadyToFight());
+console.log(squad.isReadyToMove());
+console.log(squad.getReadyToFight());
